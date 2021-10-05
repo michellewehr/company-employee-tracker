fb105d76940dd76675c37408256f463b0f86c384
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-05 15:30:47 -0400

    added avlues for roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,19 @@ VALUES
 ('Sales'),
 ('Finance'),
 ('Human Resources');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+('Sales representative', '50571', '2'),
+('Sales consultant', '65036', '2'),
+('Sales manager', '120030', '2'),
+('Marketing Manager', '117151', '1'),
+('Marketing Associate', '58935', '1'),
+('Marketing Trainee', '37633', '1'),
+('Financial Manager', '127990', '3'),
+('Accountant', '70500', '3'),
+('Financial analyst', '85660', '3'),
+('HR Manager', '115698', '4'),
+('HR Associate', '54681', '4'),
+('HR Analyst', '66625', '4'),
+('Recruiter', '50430', '4');
