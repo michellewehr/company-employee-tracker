14f5945bf2149e91ba92c8293a8abc99d2d26320
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-05 14:58:47 -0400

    installed dependencies and created department and role tables

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
+);
