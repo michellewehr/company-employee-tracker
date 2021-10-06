d06e7f8a72a3bc23809b060c87e62dfae6614415
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-06 13:35:10 -0400

    having trouble with syntax to create table

--- db/schema.sql
+++ db/schema.sql
@@ -1,43 +1,50 @@
+DROP DATABASE IF EXISTS company;
+CREATE DATABASE company;
+
 USE company;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS managers;
-DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
-    dep_name VARCHAR(30) NOT NULL, 
+    id INTEGER AUTO_INCREMENT, 
+    dep_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id) 
 );
 
 CREATE TABLE roles (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    id INTEGER AUTO_INCREMENT, 
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INTEGER,
-    FOREIGN KEY (department_id) 
+    PRIMARY KEY (id),
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_id) 
         REFERENCES departments(id) 
         ON DELETE SET NULL
 );
 
 CREATE TABLE managers (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    id INTEGER AUTO_INCREMENT, 
     manager_name VARCHAR(70);
     department_managed VARCHAR(30),
-    FOREIGN KEY (department_managed)
+    PRIMARY KEY (id),
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_managed)
          REFERENCES departments(dep_name)
          ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY, 
+    id INTEGER AUTO_INCREMENT, 
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
     role_id INTEGER, 
     manager_id INTEGER, 
-    FOREIGN KEY (role_id)
-         REFERENCES roles(id) 
-         ON DELETE SET NULL,
-    FOREIGN KEY(manager_id) 
+    PRIMARY KEY (id),
+    CONSTRAINT fk_role
+        FOREIGN KEY (role_id)
+        REFERENCES roles(id),
+        ON DELETE SET NULL,
+    CONSTRAINT fk_manager
+        FOREIGN KEY(manager_id) 
         REFERENCES managers(id)
         ON DELETE SET NULL;
-)
+);
